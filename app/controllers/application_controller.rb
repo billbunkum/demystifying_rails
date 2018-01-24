5fb5fd8aca3398486d0310fd6cc1f9ad178c5694
billbunkum <billbunkum@billbunkums-MacBook-Air.local>
2018-01-24 15:35:11 +0800

    first commit; add route action and view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+	def hello_world
+    # 'plain' is 'text/plain'
+		# render( {:plain => 'hello world'} )
+        # render({plain: 'hello world'})
+        # render plain: 'hello world'
+    # 'inline' is 'text/html'
+    	# render({:inline => '<em>hello world</em>'})
+    	# binding.pry
+    # using a 'app/views'
+    	# renders from ../views/application/
+    	render('application/hello_world')
+    # same as the following
+    	# render inline: File.read('app/views/application/hello_world.html')
+
+	end
 end
